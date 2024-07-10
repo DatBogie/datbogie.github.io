7ab0702cf93bf8efcc5e9eed5b4928c872944a25
Dat Bogie <89746947+DatBogie@users.noreply.github.com>
2024-07-09 21:20:01 -0400

    Update index.js (Animation)

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function ClickerHTML() {
-    window.location.replace("https://datbogie.github.io/ClickerHTML");
+    let x = document.getElementById("ClickerHTML");
+    if (x) {
+        x.style.scale = 20;
+        x.style.opacity = 0;
+        x.style.position = "fixed";
+        x.style.zIndex = 1;
+    };
+
+    setTimeout(function() {
+        window.location.replace("https://datbogie.github.io/ClickerHTML");
+    }, 200);
 };
